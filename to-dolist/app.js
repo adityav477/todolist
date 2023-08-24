8c2c90034bb40ea5d612fcc2a21b8cdee7f3fb3f
aditya <adityavish126@gmail.com>
2023-08-24 19:09:03 +0530

    add /work

--- to-dolist/app.js
+++ to-dolist/app.js
@@ -8,7 +8,8 @@ app.use(express.static("public"));
 
 app.set('view engine', "ejs");
 
-var listitems = ["bring food","cook food","eat food"];
+var listitems = ["bring food", "cook food", "eat food"];
+var workitems = [];
 
 app.get("/", function (req, res) {
     var date = new Date();//this givest the long format with gmt and all
@@ -19,19 +20,32 @@ app.get("/", function (req, res) {
     day = date.toLocaleDateString("en-us", options);
     // console.log(day);
 
-    res.render("list", { day: day, listitems: listitems });
+    res.render("list", { listtitle: day, listitems: listitems });
     // console.log(listitems);
 })
 
 app.post("/", function (req, res) {
     // console.log(req.body);
-    listitem = req.body.item;
-    // console.log(`listitem is ${listitem}`);
-    listitems.push(listitem);
-    res.redirect("/");
+    if (req.body.button === "Work") {
+        workitems.push(req.body.item);
+        res.redirect("/work");
+    }
+    else {
+        listitem = req.body.item;
+        // console.log(`listitem is ${listitem}`);
+        listitems.push(listitem);
+        res.redirect("/");
+    }
 })
 
-// app.get("/work",function (req,res) {})
+app.get("/work", function (req, res) {
+    res.render("list", { listtitle: "Work", listitems: workitems });
+})
+
+// app.post("/work", function (req, res) {
+//     workitems.push(req.body.item);
+//     res.redirect("/work");
+// })
 
 app.listen(3000, function () {
     console.log("Server is working");
